f0f4c84dbc03d5f401b601b9c5298debae3301ae
Jack <jackeabernathy@gmail.com>
2019-02-05 15:14:32 -0700

    added hobbies

--- group.c
+++ group.c
@@ -9,6 +9,7 @@ int Jack(void){
 printf("Jack Abernathy\n");
 printf("Year: \"Junior\" at University of Montana\n");
 printf("Major: Double major in Computer Science and Math\n");
+printf("Hobbies: Gaming, reading, and writing anything fantasy related. Occasional sleep.\n");
 
 }
 int Adrian(void){

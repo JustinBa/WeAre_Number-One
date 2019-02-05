bd6157868526d0bade73dd7d71021f0f9fb9c437
1825l <1825l@10.17.196.235>
2019-02-05 15:05:01 -0700

    Repush changes about me

--- group.c
+++ group.c
@@ -12,7 +12,12 @@ int Adrian(void){
 
 }
 int Justin(void){
-
+	printf("Justin Bancale");
+	printf("Freshman");
+	printf("Computer Science");
+	printf("Computers, Soldering, Tech, Anime, Manga");
+	printf("Missoula Montana");
+	printf("Ive sold about $25,000 in popcorn");
 }
 int William(void){
 

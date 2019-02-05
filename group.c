cc4d9dfdfd3d33c86880f3ae02411532e321ebb6
1825l <1825l@10.17.196.235>
2019-02-05 14:53:18 -0700

    added stuff

---
+++ group.c
@@ -0,0 +1,22 @@
+int main(void){
+	Jack();
+	Adrian();
+	Justin();
+	William();
+	Jacob();
+}
+int Jack(void){
+
+}
+int Adrian(void){
+
+}
+int Justin(void){
+
+}
+int William(void){
+
+}
+int Jacob(void){
+
+}

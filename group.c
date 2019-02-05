b0a2b576be371116103c829ed0c26bcdb70d746a
Adrian <adrian.ashwah@umontana.edu>
2019-02-05 15:02:35 -0700

    4th commit

--- group.c
+++ group.c
@@ -13,7 +13,8 @@ int Adrian(void){
     printf("2nd year in school\n");
     printf("Majoring in CS and IT\n");
     printf("Playing the Mandolin, Soccer, and Hiking\n");
-    printf("Grew up in Siberia, Russia.\n")
+    printf("Grew up in Siberia, Russia.\n");
+    printf("Kayaked in the open sea for 5 days!\n");
 
 }
 int Justin(void){
